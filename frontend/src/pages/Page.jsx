5f8575c7cd54713b8efd3311a68739270edc7f11
abushe136 <amir.mesfin136@gmail.com>
2025-11-24 03:47:40 +0300

    work on the  ico and set up our header part

--- frontend/src/pages/Page.jsx
+++ frontend/src/pages/Page.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import Header from '../components/header'
 import Hero from '../components/hero/Hero'
 import About from '../components/about/About'
 import Portfolio from '../components/portfolio/Portfolio'
@@ -8,6 +7,7 @@ import Services from '../components/Services/Services'
 import Contact from '../components/contact/Contact'
 import Blog from '../components/blog/Blog'
 import Footer from '../components/Footer/Footer'
+import Header from '../components/header/Header'
 export default function Page() {
   return (
     <div>
